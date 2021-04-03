e13b1ac8fe1fe766d3dc740e5a1ae246c450f202
arpit <arpit@LAPTOP-K9R4RSIU>
2021-04-03 23:37:16 +0530

    Maaa Push Selenium Practice Code.

--- Selenium/src/test/java/Interview/SetWindowSize.java
+++ Selenium/src/test/java/Interview/SetWindowSize.java
@@ -1,10 +1,31 @@
 package Interview;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SetWindowSize {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\arpit\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.getWindowHandle();
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+        driver.get("https://www.facebook.com/");
+        
+        Dimension d= new Dimension(200,300);
+        
+        driver.manage().window().setSize(d);
+        driver.close();
+        
+		
+		
+		
+		
 	}
 
 }
