8dc334557e776b8bfaafab818384cb7b405a391f
arpit <arpit@LAPTOP-K9R4RSIU>
2021-04-03 23:01:17 +0530

    Maaa

---
+++ Selenium/src/test/java/Practice/SwapNumber.java
@@ -0,0 +1,20 @@
+package Practice;
+
+public class SwapNumber {
+
+	public static void main(String[] args) {
+		int a=10,b=20,t;
+		
+		
+		System.out.println("Before Change"+a+" "+b);
+		
+		t=a;
+		a=b;
+		b=t;
+		
+		System.out.print("After change " +a+ " " +b);
+		
+
+	}
+
+}
